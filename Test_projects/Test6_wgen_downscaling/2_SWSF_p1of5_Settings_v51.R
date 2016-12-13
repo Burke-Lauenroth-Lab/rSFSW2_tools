46669e99f5ed1ab9dc661154b0989c1ece0c45ba
dschlaep <daniel.schlaepfer@unibas.ch>
2016-12-13 17:27:48 +0100

    Update parts 1 to reflect parallel-backend update to 'cluster'

--- Test_projects/Test6_wgen_downscaling/2_SWSF_p1of5_Settings_v51.R
+++ Test_projects/Test6_wgen_downscaling/2_SWSF_p1of5_Settings_v51.R
@@ -48,7 +48,7 @@ minVersionRsoilwat <- "1.1.4"
 minVersion_dbWeather <- "3.1.0"
 use_rcpp <- TRUE
 num_cores <- 4
-parallel_backend <- "snow" #"snow" or "multicore" or "mpi"
+parallel_backend <- "cluster" # "cluster" (via package 'parallel') or "mpi" (via 'Rmpi')
 parallel_runs <- !interactive()
 
 #------Computation time requests------#
